b1e730c774878088a5bc365fca43648cad242047
Matic, Andrija <andrija.matic42@gmail.com>
2025-05-29 18:56:50 +0200

    Update backend file with hardcoded values

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name
-    storage_account_name = var.storage_account_name
+    resource_group_name  = "RG"
+    storage_account_name = "tfstate744amkeycloak"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    tenant_id            = var.tenant_id
-    subscription_id      = var.subscription_id
+    tenant_id            = "568c7c02-abcf-4a01-bf1a-712dfa387476"
+    subscription_id      = "712e60af-6f36-4fb3-b21d-bd697b17fa16"
   }
 }
